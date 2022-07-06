b6b9b0dc03923706e652f8e87ebe79893b7487d1
Umut Ramazan <umutramazan26@gmail.com>
2022-07-06 11:30:45 +0300

    Consol Uygulaması Bitti Çalıştırma Kodu "App:findNumber"

--- src/Command/FinCommand.php
+++ src/Command/FinCommand.php
@@ -1,1 +1,45 @@
 <?php
+namespace  App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class FinCommand extends Command
+{
+    /**
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('App:findNumber')
+            ->setDescription('Girilen sayıların arasından en büyüğünü ve en küçük olan sayıyı size geri döndürür.')
+            ->addArgument('value',InputArgument::IS_ARRAY,'Sayılar : ')
+            ;
+
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $value= $input->getArgument('value');
+        $io = new SymfonyStyle($input,$output);
+
+        $io->title('Find Command');
+        $io->section('Girilen sayıların arasından en büyüğünü ve en küçük olan sayıyı size geri döndürür.');
+        $io->note('Sayıları Boşluk  ile ayırın!');
+        $io->success('En Büyük Sayı :'.max($value));
+        $io->warning('En Küçük Sayı :'.min($value));
+
+
+
+        return Command::SUCCESS;
+    }
+}
